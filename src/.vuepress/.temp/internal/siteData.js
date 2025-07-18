be77e7520dd3d4ec746dd4a588834f0a3cd06e92
ryan.he <hhc92611@gmail.com>
2025-07-18 20:04:48 +0800

    chapter0

--- src/.vuepress/.temp/internal/siteData.js
+++ src/.vuepress/.temp/internal/siteData.js
@@ -1,14 +1,1 @@
 export const siteData = JSON.parse("{\"base\":\"/Ascend310/\",\"lang\":\"zh-CN\",\"title\":\"主页\",\"description\":\"vuepress-theme-hope 的文档演示\",\"head\":[],\"locales\":{}}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateSiteData) {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ siteData }) => {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  })
-}
